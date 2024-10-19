2f58537a09ee0a215c7a66dc167bbc500ad028d0
“[firstname <[ahammid9@gmail.com]>
2024-10-19 00:57:13 -0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className='home'> 
+        <h1><b><i><u>This is my first assignment</u></i></b></h1>
+         
+      
+    </div>
+  )
+}
+
+export default Homepage
